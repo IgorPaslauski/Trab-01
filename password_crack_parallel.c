7ee4ad66e9bc816f1e3566b6eadabf2196f0d274
Igor Paslauski <io006235@cfjl.com.br>
2025-08-25 15:49:33 -0300

    melhora de performance para usar charset dinamico

--- password_crack_parallel.c
+++ password_crack_parallel.c
@@ -4,45 +4,72 @@
 #include <pthread.h>
 #include <time.h>
 #include <stdatomic.h>
+#include <ctype.h>
 
-#define CONJUNTO "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=[]{}|;:,.<>?"
-#define TAM_CONJUNTO (sizeof(CONJUNTO) - 1)
+#define CONJUNTO_NUMERICOS "0123456789"
+#define CONJUNTO_COMPLETO "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=[]{}|;:,.<>?"
+#define CONJUNTO_ALFABETO "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()_+-=[]{}|;:,.<>?"
 #define TAM_MAX_SENHA 100
 
 char *senha_alvo;
 int tam_senha;
 atomic_int achou = 0;
 atomic_ullong tentativas = 0;
+const char *conjunto_atual;
+int tam_conjunto;
+
+void definir_charset()
+{
+    int tem_alfabeto = 0;
+    int tem_numero = 0;
+
+    for (int i = 0; i < tam_senha; i++)
+    {
+        if (isdigit((unsigned char)senha_alvo[i]))
+            tem_numero = 1;
+        else
+            tem_alfabeto = 1;
+    }
+
+    if (tem_alfabeto && tem_numero)
+        conjunto_atual = CONJUNTO_COMPLETO;
+    else if (tem_numero)
+        conjunto_atual = CONJUNTO_NUMERICOS;
+    else
+        conjunto_atual = CONJUNTO_ALFABETO;
+
+    tam_conjunto = strlen(conjunto_atual);
+}
 
 void *forca_bruta(void *arg)
 {
-    int id_thread = ((int *)arg)[0];
-    int qtd_threads = ((int *)arg)[1];
+    int *args = (int *)arg;
+    int id_thread = args[0];
+    int qtd_threads = args[1];
     free(arg);
 
-    long long total_combinacoes = 1;
+    unsigned long long total_combinacoes = 1;
     for (int i = 0; i < tam_senha; i++)
-        total_combinacoes *= TAM_CONJUNTO;
+        total_combinacoes *= tam_conjunto;
 
     char tentativa[TAM_MAX_SENHA + 1];
 
-    for (long long i = id_thread; i < total_combinacoes && !achou; i += qtd_threads)
+    for (unsigned long long i = id_thread; i < total_combinacoes && !achou; i += qtd_threads)
     {
-        long long indice = i;
+        unsigned long long indice = i;
         for (int j = 0; j < tam_senha; j++)
         {
-            tentativa[j] = CONJUNTO[indice % TAM_CONJUNTO];
-            indice /= TAM_CONJUNTO;
+            tentativa[j] = conjunto_atual[indice % tam_conjunto];
+            indice /= tam_conjunto;
         }
         tentativa[tam_senha] = '\0';
 
-        tentativas++;
+        atomic_fetch_add(&tentativas, 1);
 
         if (memcmp(tentativa, senha_alvo, tam_senha) == 0)
         {
-            if (!achou)
+            if (atomic_exchange(&achou, 1) == 0)
             {
-                achou = 1;
                 printf("Senha encontrada: %s\n", tentativa);
                 printf("Tentativas realizadas: %llu\n", (unsigned long long)tentativas);
             }
@@ -58,30 +85,28 @@ void medir_tempo(int qtd_threads)
     clock_t inicio, fim;
     double tempo_gasto;
 
-    pthread_t *threads = (pthread_t *)malloc(qtd_threads * sizeof(pthread_t));
+    pthread_t *threads = malloc(qtd_threads * sizeof(pthread_t));
 
     inicio = clock();
 
     for (int i = 0; i < qtd_threads; i++)
     {
-        int *arg = (int *)malloc(sizeof(int) * 2);
+        int *arg = malloc(2 * sizeof(int));
         arg[0] = i;
         arg[1] = qtd_threads;
 
         if (pthread_create(&threads[i], NULL, forca_bruta, arg) != 0)
         {
             perror("Erro ao criar thread");
-            return;
+            exit(EXIT_FAILURE);
         }
     }
 
     for (int i = 0; i < qtd_threads; i++)
-    {
         pthread_join(threads[i], NULL);
-    }
 
     fim = clock();
-    tempo_gasto = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+    tempo_gasto = (double)(fim - inicio) / CLOCKS_PER_SEC;
 
     printf("Tempo com %d threads: %.2f segundos\n", qtd_threads, tempo_gasto);
 
@@ -90,16 +115,22 @@ void medir_tempo(int qtd_threads)
 
 int main()
 {
-    int qtd_threads;
+    senha_alvo = malloc(TAM_MAX_SENHA * sizeof(char));
 
     printf("Digite a senha: ");
-    senha_alvo = (char *)malloc(TAM_MAX_SENHA * sizeof(char));
-    fgets(senha_alvo, TAM_MAX_SENHA, stdin);
+    if (!fgets(senha_alvo, TAM_MAX_SENHA, stdin))
+    {
+        perror("Erro ao ler senha");
+        return EXIT_FAILURE;
+    }
     senha_alvo[strcspn(senha_alvo, "\n")] = '\0';
 
     tam_senha = strlen(senha_alvo);
+    definir_charset();
 
-    for (qtd_threads = 1; qtd_threads <= 8; qtd_threads++)
+    printf("Charset escolhido: %s\n", conjunto_atual);
+
+    for (int qtd_threads = 1; qtd_threads <= 8; qtd_threads++)
     {
         achou = 0;
         tentativas = 0;
